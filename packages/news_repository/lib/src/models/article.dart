e820a3c7d7198ecabf8d7726ef8805fa18183b1c
danarputra4648 <wakwaujaim@gmail.com>
2021-10-06 14:25:31 +0700

    Add news repository

---
+++ packages/news_repository/lib/src/models/article.dart
@@ -0,0 +1,101 @@
+/// A class Article will be used as model
+/// to decode JSON to this class
+class Article {
+  /// Constructor
+  const Article({
+    this.source,
+    this.author,
+    this.title,
+    this.description,
+    this.url,
+    this.urlImage,
+    this.publishAt,
+    this.content,
+  });
+
+  /// Function will convert JSON map and return this class
+  factory Article.fromMap(Map<String, dynamic> map) {
+    return Article(
+      source: Source.fromMap(map['source'] as Map<String, Object?>),
+      author: map['author'] as String,
+      title: map['title'] as String,
+      description: map['description'] as String,
+      url: map['url'] as String,
+      urlImage: map['urlImage'] as String,
+      publishAt: map['publishAt'] as String,
+      content: map['content'] as String,
+    );
+  }
+
+  /// Source of News is provided
+  final Source? source;
+
+  /// Author of this article
+  final String? author;
+
+  /// Title of this article
+  final String? title;
+
+  /// Description of this article
+  final String? description;
+
+  /// Link url of this article
+  final String? url;
+
+  /// Image url of this article
+  final String? urlImage;
+
+  /// Date of this article published
+  final String? publishAt;
+
+  /// Conten of this article
+  /// but still a characters length existed
+  final String? content;
+
+  /// Function used for saving data to database
+  /// if necessarry
+  Map<String, Object?> toMap() {
+    return {
+      'source': source?.toMap(),
+      'author': author,
+      'title': title,
+      'description': description,
+      'url': url,
+      'urlImage': urlImage,
+      'publishAt': publishAt,
+      'content': content,
+    };
+  }
+}
+
+/// Sub-class Article for Source
+class Source {
+  /// Constructor
+  const Source({
+    this.id,
+    this.name,
+  });
+
+  /// Function will convert JSON map and return this class
+  factory Source.fromMap(Map<String, dynamic> map) {
+    return Source(
+      id: map['id'] as int,
+      name: map['name'] as String,
+    );
+  }
+
+  /// Id of Source
+  final int? id;
+
+  /// Name of source
+  final String? name;
+
+  /// Function used for saving data to database
+  /// if necessarry
+  Map<String, Object?> toMap() {
+    return {
+      'id': id,
+      'name': name,
+    };
+  }
+}
