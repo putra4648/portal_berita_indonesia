4ef531040d397597181a12a28fd490b97fbf4872
danarputra4648 <wakwaujaim@gmail.com>
2021-10-07 09:23:46 +0700

    Add search screen

---
+++ lib/search/view/search.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Search extends StatefulWidget {
+  const Search({Key? key}) : super(key: key);
+
+  @override
+  SearchState createState() => SearchState();
+}
+
+class SearchState extends State<Search> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text('Empty'),
+    );
+  }
+}
