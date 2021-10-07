3d7f18d8428121c9faad7f164f83a29832c248d7
danarputra4648 <wakwaujaim@gmail.com>
2021-10-07 07:59:08 +0700

    Add enums

---
+++ packages/news_repository/lib/src/enums/category.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: public_member_api_docs
+
+/// Category type in API
+enum CategoryType {
+  business,
+  entertainment,
+  general,
+  health,
+  science,
+  sports,
+  technology
+}
