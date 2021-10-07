3c5625fe6d462539da0f70194c5ea9f35af35d52
danarputra4648 <wakwaujaim@gmail.com>
2021-10-07 11:23:36 +0700

    Fix drawer and theme

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -9,25 +9,70 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:portal_berita_indonesia/home/home.dart';
 import 'package:portal_berita_indonesia/l10n/l10n.dart';
+import 'package:portal_berita_indonesia/profile/profile.dart';
+import 'package:portal_berita_indonesia/search/search.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({Key? key}) : super(key: key);
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Portal Berita',
       theme: ThemeData(
-        primaryColor: Colors.black,
+        typography: Typography.material2018(),
+        canvasColor: Colors.white,
         visualDensity: VisualDensity.compact,
-        colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.white),
+        colorScheme: const ColorScheme.light(
+          primary: Colors.white,
+        ),
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          selectedItemColor: Colors.black,
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            primary: Colors.black,
+            padding: EdgeInsets.zero,
+          ),
+        ),
       ),
       localizationsDelegates: const [
         AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
       ],
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const Home(),
+      home: Scaffold(
+        drawer: Drawer(
+          child: Column(
+            children: const [
+              DrawerHeader(child: Text('Hello guys Radal here'))
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            _currentIndex = index;
+            setState(() {});
+          },
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: ''),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: ''),
+          ],
+        ),
+        body: [const Home(), const Search(), const Profile()]
+            .elementAt(_currentIndex),
+      ),
     );
   }
 }
