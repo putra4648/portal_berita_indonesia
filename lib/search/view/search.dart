de07eea1008f02dcb7e97998a0cc42d246baa37f
danarputra4648 <wakwaujaim@gmail.com>
2021-10-07 11:24:36 +0700

    Building search scree

--- lib/search/view/search.dart
+++ lib/search/view/search.dart
@@ -10,8 +10,46 @@ class Search extends StatefulWidget {
 class SearchState extends State<Search> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Empty'),
+    final textTheme = Theme.of(context).textTheme;
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Scaffold.of(context).openDrawer();
+          },
+          icon: const Icon(
+            Icons.menu,
+            color: Colors.black,
+          ),
+        ),
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Temukan',
+              style: textTheme.headline3
+                  ?.copyWith(color: Colors.black, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'Berita di seluruh ASEAN',
+              style: textTheme.caption,
+            ),
+            const TextField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search_outlined),
+                border: InputBorder.none,
+                hintText: 'Cari',
+                enabledBorder: InputBorder.none,
+                errorBorder: InputBorder.none,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
