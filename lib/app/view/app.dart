488d60219df50b35bbd96bd41ab24e7909bff58e
danarputra4648 <wakwaujaim@gmail.com>
2021-10-29 20:34:29 +0700

    Removed drawer

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -11,7 +11,6 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:news_repository/news_repository.dart';
 import 'package:portal_berita_indonesia/category/bloc/news_category_bloc.dart';
 import 'package:portal_berita_indonesia/detail/bloc/recommendation_news_bloc.dart';
-import 'package:portal_berita_indonesia/detail/view/detail.dart';
 import 'package:portal_berita_indonesia/home/bloc/breaking_news_bloc.dart';
 import 'package:portal_berita_indonesia/home/home.dart';
 import 'package:portal_berita_indonesia/injection_container.dart';
@@ -57,27 +56,12 @@ class _AppState extends State<App> {
           title: 'Portal Berita',
           darkTheme: ThemeData.dark(),
           theme: ThemeData.light(),
-          onGenerateRoute: (settings) {
-            if (settings.name == Detail.route) {}
-          },
           localizationsDelegates: const [
             AppLocalizations.delegate,
             GlobalMaterialLocalizations.delegate,
           ],
           supportedLocales: AppLocalizations.supportedLocales,
           home: Scaffold(
-            drawer: Drawer(
-              child: ListView(
-                children: const [
-                  DrawerHeader(
-                    decoration: BoxDecoration(
-                      color: Colors.red,
-                    ),
-                    child: Text('Hello guys Radal here'),
-                  ),
-                ],
-              ),
-            ),
             bottomNavigationBar: BottomNavigationBar(
               currentIndex: currentIndex,
               onTap: (index) {
